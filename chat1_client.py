bb089123713ef97a75c22dff5e6f9ea7a61c1c9c
kolomame <221205086@ccmailg.meijo-u.ac.jp>
2024-03-11 22:00:36 +0900

    online

---
+++ chat1_client.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+import time
+
+def protocol_header(usernamelen, data_length):
+    return usernamelen.to_bytes(1, "big") + data_length.to_bytes(4, "big")
+
+
+def send_message(sock, server_address, server_port, name):
+    while True:
+        sendmessage = input("")
+        print("\033[1A\033[1A") 
+        print("You: " + sendmessage)
+        name_bits = name.encode('utf-8')
+        message_bits = sendmessage.encode('utf-8')
+        header = protocol_header(len(name_bits), len(message_bits))
+
+        sock.sendto(header, (server_address, server_port))
+        sock.sendto(name_bits, (server_address, server_port))
+        sock.sendto(message_bits, (server_address, server_port))
+        time.sleep(0.5)
+
+def receive_message(sock):
+    try:
+        while True:
+            receive = sock.recvfrom(1024)[0].decode('utf-8')
+            print(receive)
+
+
+    except Exception as e:
+        print("Error in receive_message:", e)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+server_address = input("Type in the server's address to connect to: ")
+server_port = 9001
+address = ''
+port = 9050
+
+sock.bind((address,port))
+
+name = input("My name is: ")
+
+send_thread = threading.Thread(target=send_message, args=(sock, server_address, server_port, name))
+receive_thread = threading.Thread(target=receive_message, args=(sock,))
+
+
+send_thread.start()
+receive_thread.start()
+
+send_thread.join()
+receive_thread.join()
+sock.close()
---
+++ chat1_server.py
@@ -0,0 +1,56 @@
+import socket
+import time
+import threading
+
+def remove_client(user_info, usertime):
+    while True:
+        current_time = time.time()
+        for address, t in usertime.items():
+            if current_time - t >= 10:
+                if address in user_info:
+                    del user_info[address]
+
+        time.sleep(0.5)
+
+
+def sendreceive(sock, user_info, usertime):
+    while True:
+        print('\nwaiting to receive message')
+        header, address = sock.recvfrom(8)
+
+
+        name_length = int.from_bytes(header[:1], "big")
+        data_length = int.from_bytes(header[1:], "big")
+
+        username_bits, address = sock.recvfrom(name_length)
+
+        username = username_bits.decode('utf-8')
+        user_info[address] = username
+        usertime[address] = time.time()
+        data_bits, address = sock.recvfrom(data_length)
+        data = data_bits.decode('utf-8')
+        print(f"name: {username}, data: {data}")
+
+        for key in user_info:
+            if key == address: continue
+            sock.sendto((f"{username}: {data}").encode('utf-8'), key)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = '0.0.0.0'
+server_port = 9001
+print('starting up on port {}'.format(server_port))
+
+sock.bind((server_address, server_port))
+user_info = {} #user_info[address] = username
+usertime = {} #usertime[address] = time.time()
+
+send_thread = threading.Thread(target=sendreceive, args=(sock, user_info, usertime))
+remove_thread = threading.Thread(target=remove_client, args=(user_info, usertime))
+
+send_thread.start()
+remove_thread.start()
+
+send_thread.join()
+remove_thread.join()
